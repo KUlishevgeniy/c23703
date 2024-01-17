6f872f53d943cfd44a99e6ab83b901f9d4c387e9
MichaelEugen <153594023+MichaelEugen@users.noreply.github.com>
2024-01-17 22:35:52 +0300

    Add files via upload

---
+++ Vasiliev/05/burger.py
@@ -0,0 +1,22 @@
+def bulka():
+    print('Добавляем булочку с кунжутом')
+
+
+def gov_cotleta():
+    print('Добавляем говяжью котлету')
+
+
+def ogur():
+    print('Добавляем огурец')
+
+
+def tomato():
+    print('Добавляем помидор')
+
+
+def mayonezo():
+    print('Добавляем майонез')
+
+
+def cheese():
+    print('Добавляем сыр')
