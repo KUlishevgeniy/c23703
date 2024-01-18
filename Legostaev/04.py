4083251b941faf315cd8f8c1300dd986dcf9c4f0
staslegostaev <stas.legostaev.00@bk.ru>
2024-01-18 21:05:53 +0300

    Create 04.py

---
+++ Legostaev/04.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Вывести на консоль жителей комнат (модули room_1 и room_2)
+# Формат: В комнате room_1 живут: ...
+
+# TODO здесь ваш код
+
+from room_1 import folks as folks1
+from room_2 import folks as f
+
+print(f'В комнате room_1 живут: {folks1}')
+print(f'В комнате room_2 живут: {folks2}')
