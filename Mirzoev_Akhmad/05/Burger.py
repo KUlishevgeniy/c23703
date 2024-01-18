3e4cd42d21566a785c84677f799a662a31146704
mizevv <152420437+mizevv@users.noreply.github.com>
2024-01-18 15:28:36 +0300

    Add files via upload

---
+++ Mirzoev_Akhmad/05/Burger.py
@@ -0,0 +1,22 @@
+def bulka():
+    print('+булка')
+
+
+def gov_cotleta():
+    print('+котлета')
+
+
+def ogur():
+    print('+огурец')
+
+
+def tomato():
+    print('+помидор')
+
+
+def mayonezo():
+    print('+майонез')
+
+
+def cheese():
+    print('+сыр')
